5b39c3693daff29b1cee0c7de78226daadaff956
Humberto Rodrigues <hpereira@senseilabs.com>
2022-01-24 13:17:15 -0500

    change keyvault package

--- src/main/java/com/example/demo/ConfigServerApplication.java
+++ src/main/java/com/example/demo/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.senseilabs.configserver;
+package com.mellon.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
